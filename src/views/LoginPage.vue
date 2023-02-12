bdc01939caba57e8af7c30843715b7b9195e9237
marcelo <marcelo@contassconsultoria.com.br>
2023-02-12 10:01:24 -0300

    iniciando

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <form @submit.prevent="submit">
+      <input type="text" v-model="username" placeholder="Username" />
+      <input type="password" v-model="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    submit() {
+      // Make a POST request to the Laravel backend
+    }
+  }
+};
+</script>
